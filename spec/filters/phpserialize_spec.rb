62c09653e8f241c604d1d8ce8d2fe662e8c5d386
Roland Angerer <roland.angerer@beenergised.com>
2017-05-10 14:10:37 +0200

    simplify tests

--- spec/filters/phpserialize_spec.rb
+++ spec/filters/phpserialize_spec.rb
@@ -13,7 +13,7 @@ describe LogStash::Filters::Phpserialize do
     CONFIG
     end
 
-    sample("message" => 's:11:"Hello World";') do
+    sample('s:11:"Hello World";') do
       expect(subject).to include('php')
       expect(subject.get('php')).to eq('Hello World')
     end
@@ -72,16 +72,16 @@ describe LogStash::Filters::Phpserialize do
     let(:config) do <<-CONFIG
       filter {
         phpserialize {
-          source => "php_in"
-          target => "php_out"
+          source => "in"
+          target => "php"
         }
       }
     CONFIG
     end
 
-    sample("php_in" => 'i:42;') do
-      expect(subject).to include('php_out')
-      expect(subject.get('php_out')).to eq(42)
+    sample("in" => 'i:42;') do
+      expect(subject).to include('php')
+      expect(subject.get('php')).to eq(42)
     end
   end
 end
